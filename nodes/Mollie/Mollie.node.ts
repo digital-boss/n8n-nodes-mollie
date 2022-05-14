7b5cde0086de959a4a805cae7b7c842613630870
xlincw0w <khaled.khazem@yahoo.com>
2022-05-14 20:18:11 +0100

    Optional parameters included

--- nodes/Mollie/Mollie.node.ts
+++ nodes/Mollie/Mollie.node.ts
@@ -1,6 +1,4 @@
-import {
-	IExecuteFunctions
-} from 'n8n-core';
+import { IExecuteFunctions } from "n8n-core";
 
 import {
 	IDataObject,
@@ -8,12 +6,9 @@ import {
 	INodeType,
 	INodeTypeDescription,
 	NodeApiError,
-} from 'n8n-workflow';
+} from "n8n-workflow";
 
-import {
-	mollieApiRequest,
-	simplify
-} from './GenericFunctions';
+import { mollieApiRequest, simplify } from "./GenericFunctions";
 
 import {
 	methodsFields,
@@ -22,59 +17,99 @@ import {
 	paymentLinksOperations,
 	paymentsFields,
 	paymentsOperations,
-} from './descriptions';
+} from "./descriptions";
 
-import { version } from '../version';
+import { version } from "../version";
+
+const parseParams = (params: any): any => {
+	const optionalLength = Object.keys(params).length;
+	const optionalData = Object.keys(params).map(
+		(firstKey: string, firstIndex: number) => {
+			let rowString = "";
+			if (firstIndex === 0) rowString = "?";
+
+			let value = params[firstKey];
+			if (
+				value.slice(0, 1) === "{" &&
+				value.slice(value.length - 1, value.length) === "}"
+			) {
+				const parsedValue = JSON.parse(value);
+				const parsedValueLength = Object.keys(parsedValue).length;
+				const attributes = Object.keys(parsedValue).map(
+					(keySecond: string, secondIndex: number) => {
+						let attributeString = "";
+
+						attributeString += `${firstKey}[${keySecond}]=${parsedValue[keySecond]}`;
+						if (secondIndex !== parsedValueLength - 1)
+							attributeString = `${attributeString}&`;
+
+						return attributeString;
+					}
+				);
+
+				value = attributes.join("").replace(",", "");
+				rowString += `${value}`;
+			} else {
+				rowString += `${firstKey}=${value}`;
+			}
+
+			if (firstIndex !== optionalLength - 1) rowString = `${rowString}&`;
+			return rowString;
+		}
+	);
+
+	return optionalData.join("").replace(",", "");
+};
 
 export class Mollie implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Mollie',
-		name: 'mollie',
-		icon: 'file:mollie.png',
-		group: ['transform'],
+		displayName: "Mollie",
+		name: "mollie",
+		icon: "file:mollie.png",
+		group: ["transform"],
 		version: 1,
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
 		description: `Consume Mollie API (v.${version})`,
 		defaults: {
-			name: 'Mollie',
-			color: '#772244',
+			name: "Mollie",
+			color: "#772244",
 		},
-		inputs: ['main'],
-		outputs: ['main'],
+		inputs: ["main"],
+		outputs: ["main"],
 		credentials: [
 			{
-				name: 'mollieApi',
+				name: "mollieApi",
 				required: true,
 			},
 		],
 		properties: [
 			{
-				displayName: 'Live Api Key',
-				name: 'isLiveKey',
+				displayName: "Live Api Key",
+				name: "isLiveKey",
 				required: true,
-				type: 'boolean',
+				type: "boolean",
 				default: true,
 			},
 			{
-				displayName: 'Resource',
-				name: 'resource',
+				displayName: "Resource",
+				name: "resource",
 				required: true,
-				type: 'options',
+				type: "options",
 				options: [
 					{
-						name: 'Payments',
-						value: 'payments',
+						name: "Payments",
+						value: "payments",
 					},
 					{
-						name: 'Payment links',
-						value: 'paymentLinks',
+						name: "Payment links",
+						value: "paymentLinks",
 					},
 					{
-						name: 'Methods',
-						value: 'methods',
+						name: "Methods",
+						value: "methods",
 					},
 				],
-				default: 'payments',
+				default: "payments",
 			},
 			...paymentsOperations,
 			...paymentsFields,
@@ -83,7 +118,6 @@ export class Mollie implements INodeType {
 			...methodsOperations,
 			...methodsFields,
 		],
-
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
@@ -91,70 +125,101 @@ export class Mollie implements INodeType {
 		let responseData;
 		const body: IDataObject = {};
 		const qs: IDataObject = {};
-		let method = '';
-		let endpoint = '';
+		let method = "";
+		let endpoint = "";
 		const returnData: IDataObject[] = [];
 
-		const isLiveKey = this.getNodeParameter('isLiveKey', 0) as boolean;
-		const resource = this.getNodeParameter('resource', 0) as string;
-		const operation = this.getNodeParameter('operation', 0) as string;
+		const isLiveKey = this.getNodeParameter("isLiveKey", 0) as boolean;
+		const resource = this.getNodeParameter("resource", 0) as string;
+		const operation = this.getNodeParameter("operation", 0) as string;
+
+		const optionalParameters: any = this.getNodeParameter(
+			"optionalParameters",
+			0
+		) as string;
 
 		for (let i = 0; i < items.length; i++) {
 			try {
 				switch (resource) {
-					case 'payments':
-						const paymentUrl = '/payments';
+					case "payments":
+						const paymentUrl = "/payments";
 						switch (operation) {
-							case 'create':
+							case "create":
 								// ----------------------------------
 								//        payments:create
 								// ----------------------------------
 								endpoint = paymentUrl;
-								method = 'POST';
+								method = "POST";
 								body.metadata = {
-									order_id: this.getNodeParameter('orderId', i) as string,
+									order_id: this.getNodeParameter("orderId", i) as string,
 								};
 								body.amount = {
-									currency: this.getNodeParameter('currency', i) as string,
-									value: (this.getNodeParameter('value', i) as string).toString(), // Ensure sending a string value
+									currency: this.getNodeParameter("currency", i) as string,
+									value: (
+										this.getNodeParameter("value", i) as string
+									).toString(), // Ensure sending a string value
 								};
-								body.description = this.getNodeParameter('description', i) as string;
-								body.redirectUrl = this.getNodeParameter('redirectUrl', i) as string;
-								Object.assign(body, this.getNodeParameter('additionalFields', i) as IDataObject);
+								body.description = this.getNodeParameter(
+									"description",
+									i
+								) as string;
+								body.redirectUrl = this.getNodeParameter(
+									"redirectUrl",
+									i
+								) as string;
+								Object.assign(
+									body,
+									this.getNodeParameter("additionalFields", i) as IDataObject
+								);
 								break;
 
-							case 'delete':
+							case "delete":
 								// ----------------------------------
 								//        payments:delete
 								// ----------------------------------
-								endpoint = paymentUrl + '/' + this.getNodeParameter('id', i) as string;
-								method = 'DELETE';
+								endpoint = (paymentUrl +
+									"/" +
+									this.getNodeParameter("id", i)) as string;
+								method = "DELETE";
 								break;
 
-							case 'get':
+							case "get":
 								// ----------------------------------
 								//        payments:get
 								// ----------------------------------
-								endpoint = paymentUrl + '/' + this.getNodeParameter('id', i) as string;
-								method = 'GET';
+								endpoint = (paymentUrl +
+									"/" +
+									this.getNodeParameter("id", i)) as string;
+								method = "GET";
 								break;
 
-							case 'list':
+							case "list":
 								// ----------------------------------
 								//        payments:list
 								// ----------------------------------
 								endpoint = paymentUrl;
-								method = 'GET';
-								Object.assign(qs, this.getNodeParameter('additionalParameters', i) as IDataObject);
+								method = "GET";
+								Object.assign(
+									qs,
+									this.getNodeParameter(
+										"additionalParameters",
+										i
+									) as IDataObject
+								);
 								break;
 
-							case 'update':
+							case "update":
 								// ----------------------------------
 								//        payments:update
 								// ----------------------------------
-								endpoint = paymentUrl + '/' + this.getNodeParameter('id', i) as string;
-								method = 'PATCH';
-								Object.assign(body, this.getNodeParameter('additionalFields', i) as IDataObject);
+								endpoint = (paymentUrl +
+									"/" +
+									this.getNodeParameter("id", i)) as string;
+								method = "PATCH";
+								Object.assign(
+									body,
+									this.getNodeParameter("additionalFields", i) as IDataObject
+								);
 								break;
 
 							default:
@@ -162,38 +227,54 @@ export class Mollie implements INodeType {
 						}
 						break;
 
-					case 'paymentLinks':
-						const paymentLinksUrl = '/payment-links';
+					case "paymentLinks":
+						const paymentLinksUrl = "/payment-links";
 						switch (operation) {
-							case 'create':
+							case "create":
 								// ----------------------------------
 								//        paymentLinks:create
 								// ----------------------------------
 								endpoint = paymentLinksUrl;
-								method = 'POST';
+								method = "POST";
 								body.amount = {
-									currency: this.getNodeParameter('currency', i) as string,
-									value: (this.getNodeParameter('value', i) as string).toString(), // Ensure sending a string value
+									currency: this.getNodeParameter("currency", i) as string,
+									value: (
+										this.getNodeParameter("value", i) as string
+									).toString(), // Ensure sending a string value
 								};
-								body.description = this.getNodeParameter('description', i) as string;
-								Object.assign(body, this.getNodeParameter('additionalFields', i) as IDataObject);
+								body.description = this.getNodeParameter(
+									"description",
+									i
+								) as string;
+								Object.assign(
+									body,
+									this.getNodeParameter("additionalFields", i) as IDataObject
+								);
 								break;
 
-							case 'get':
+							case "get":
 								// ----------------------------------
 								//        paymentLinks:get
 								// ----------------------------------
-								endpoint = paymentLinksUrl + '/' + this.getNodeParameter('id', i) as string;
-								method = 'GET';
+								endpoint = (paymentLinksUrl +
+									"/" +
+									this.getNodeParameter("id", i)) as string;
+								method = "GET";
 								break;
 
-							case 'list':
+							case "list":
 								// ----------------------------------
 								//        paymentLinks:list
 								// ----------------------------------
 								endpoint = paymentLinksUrl;
-								method = 'GET';
-								Object.assign(qs, this.getNodeParameter('additionalParameters', i) as IDataObject);
+								method = "GET";
+								Object.assign(
+									qs,
+									this.getNodeParameter(
+										"additionalParameters",
+										i
+									) as IDataObject
+								);
 
 								break;
 
@@ -203,31 +284,37 @@ export class Mollie implements INodeType {
 
 						break;
 
-					case 'methods':
-						const methodsUri = '/methods';
+					case "methods":
+						const methodsUri = "/methods";
 						switch (operation) {
-							case 'list':
+							case "list":
 								// ----------------------------------
 								//        methods:list
 								// ----------------------------------
-								endpoint = methodsUri;
-								method = 'GET';
+
+								endpoint = `${methodsUri}${parseParams(optionalParameters)}`;
+								method = "GET";
 								break;
 
-							case 'listAll':
+							case "listAll":
 								// ----------------------------------
 								//        methods:listAll
 								// ----------------------------------
-								endpoint = methodsUri + '/all';
-								method = 'GET';
+								endpoint = `${methodsUri}/all${parseParams(
+									optionalParameters
+								)}`;
+								method = "GET";
 								break;
 
-							case 'get':
+							case "get":
 								// ----------------------------------
 								//        methods:get
 								// ----------------------------------
-								endpoint = methodsUri + '/' + this.getNodeParameter('id', i) as string;
-								method = 'GET';
+								endpoint = `${methodsUri}/${this.getNodeParameter(
+									"id",
+									i
+								)}${parseParams(optionalParameters)}`;
+								method = "GET";
 								break;
 
 							default:
@@ -239,23 +326,30 @@ export class Mollie implements INodeType {
 						break;
 				}
 
-				responseData = await mollieApiRequest.call(this, method, endpoint, qs, body, isLiveKey);
+				responseData = await mollieApiRequest.call(
+					this,
+					method,
+					endpoint,
+					qs,
+					body,
+					isLiveKey
+				);
 				responseData = JSON.parse(responseData);
 
-				if (responseData.name === 'Error') {
+				if (responseData.name === "Error") {
 					throw new NodeApiError(this.getNode(), responseData);
 				}
 
-				if (operation === 'list' || operation === 'listAll') {
+				if (operation === "list" || operation === "listAll") {
 					switch (resource) {
-						case 'payments':
-							responseData = simplify(responseData, 'payments');
+						case "payments":
+							responseData = simplify(responseData, "payments");
 							break;
-						case 'paymentLinks':
-							responseData = simplify(responseData, 'payment_links');
+						case "paymentLinks":
+							responseData = simplify(responseData, "payment_links");
 							break;
-						case 'methods':
-							responseData = simplify(responseData, 'methods');
+						case "methods":
+							responseData = simplify(responseData, "methods");
 							break;
 
 						default:
--- nodes/Mollie/descriptions/MethodsDescription.ts
+++ nodes/Mollie/descriptions/MethodsDescription.ts
@@ -1,36 +1,125 @@
-import {
-	INodeProperties,
-} from 'n8n-workflow';
+import { INodeProperties } from "n8n-workflow";
+
+const localOptions = [
+	{
+		name: "en_US",
+		value: "en_US",
+	},
+	{
+		name: "en_GB",
+		value: "en_GB",
+	},
+	{
+		name: "nl_NL",
+		value: "nl_NL",
+	},
+	{
+		name: "nl_BE",
+		value: "nl_BE",
+	},
+	{
+		name: "fr_FR",
+		value: "fr_FR",
+	},
+	{
+		name: "fr_BE",
+		value: "fr_BE",
+	},
+	{
+		name: "de_DE",
+		value: "de_DE",
+	},
+	{
+		name: "de_AT",
+		value: "de_AT",
+	},
+	{
+		name: "de_CH",
+		value: "de_CH",
+	},
+	{
+		name: "es_ES",
+		value: "es_ES",
+	},
+	{
+		name: "ca_ES",
+		value: "ca_ES",
+	},
+	{
+		name: "pt_PT",
+		value: "pt_PT",
+	},
+	{
+		name: "it_IT",
+		value: "it_IT",
+	},
+	{
+		name: "nb_NO",
+		value: "nb_NO",
+	},
+	{
+		name: "sv_SE",
+		value: "sv_SE",
+	},
+	{
+		name: "fi_FI",
+		value: "fi_FI",
+	},
+	{
+		name: "da_DK",
+		value: "da_DK",
+	},
+	{
+		name: "is_IS",
+		value: "is_IS",
+	},
+	{
+		name: "hu_HU",
+		value: "hu_HU",
+	},
+	{
+		name: "pl_PL",
+		value: "pl_PL",
+	},
+	{
+		name: "lv_LV",
+		value: "lv_LV",
+	},
+	{
+		name: "lt_LT",
+		value: "lt_LT",
+	},
+];
 
 export const methodsOperations: INodeProperties[] = [
 	{
-		displayName: 'Operation',
-		name: 'operation',
+		displayName: "Operation",
+		name: "operation",
 		required: true,
-		type: 'options',
+		type: "options",
 		displayOptions: {
 			show: {
-				resource: ['methods'],
+				resource: ["methods"],
 			},
 		},
-		default: 'list',
-		description: 'The operation to perform',
+		default: "list",
+		description: "The operation to perform",
 		options: [
 			{
-				name: 'List',
-				value: 'list',
-				description: 'Retrieve all enabled payment methods',
+				name: "List",
+				value: "list",
+				description: "Retrieve all enabled payment methods",
 			},
 			{
-				name: 'List all',
-				value: 'listAll',
+				name: "List all",
+				value: "listAll",
 				description:
-					'Retrieve all payment methods that Mollie offers and can be activated by the Organization',
+					"Retrieve all payment methods that Mollie offers and can be activated by the Organization",
 			},
 			{
-				name: 'Get',
-				value: 'get',
-				description: 'Retrieve a single method by its ID',
+				name: "Get",
+				value: "get",
+				description: "Retrieve a single method by its ID",
 			},
 		],
 	},
@@ -40,26 +129,168 @@ export const methodsFields: INodeProperties[] = [
 	/*-------------------------------------------------------------------------- */
 	/*                                methods:list                             	 */
 	/* ------------------------------------------------------------------------- */
+	{
+		displayName: "Optional parameters",
+		name: "optionalParameters",
+		type: "collection",
+		placeholder: "Add Options",
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ["methods"],
+				operation: ["list"],
+			},
+		},
+		options: [
+			{
+				displayName: "Sequence type",
+				name: "sequenceType",
+				type: "options",
+				default: "",
+				options: [
+					{
+						name: "Oneoff",
+						value: "oneoff",
+					},
+					{
+						name: "First",
+						value: "first",
+					},
+					{
+						name: "Recurring",
+						value: "recurring",
+					},
+				],
+			},
+			{
+				displayName: "Locale",
+				name: "locale",
+				type: "options",
+				default: "",
+				options: localOptions,
+			},
+			{
+				displayName: "Amount",
+				name: "amount",
+				type: "json",
+				default: "",
+			},
+			{
+				displayName: "Resource",
+				name: "resource",
+				type: "options",
+				default: "",
+				options: [
+					{
+						name: "Orders",
+						value: "orders",
+					},
+					{
+						name: "Payments",
+						value: "payments",
+					},
+				],
+			},
+			{
+				displayName: "Billing country",
+				name: "billingCountry",
+				type: "string",
+				default: "",
+			},
+			{
+				displayName: "Include Wallet",
+				name: "includeWallet",
+				type: "options",
+				default: "",
+				options: [
+					{
+						name: "Apple pay",
+						value: "applepay",
+					},
+				],
+			},
+			{
+				displayName: "Order line category",
+				name: "orderLineCategory",
+				type: "string",
+				default: "",
+			},
+		],
+	},
 
 	/*-------------------------------------------------------------------------- */
 	/*                                methods:listAll                            */
 	/* ------------------------------------------------------------------------- */
-
+	{
+		displayName: "Optional parameters",
+		name: "optionalParameters",
+		type: "collection",
+		placeholder: "Add Options",
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ["methods"],
+				operation: ["listAll"],
+			},
+		},
+		options: [
+			{
+				displayName: "Locale",
+				name: "locale",
+				type: "options",
+				default: "",
+				options: localOptions,
+			},
+			{
+				displayName: "Amount",
+				name: "amount",
+				type: "json",
+				default: "",
+			},
+		],
+	},
 	/*-------------------------------------------------------------------------- */
 	/*                                methods:get                             	 */
 	/* ------------------------------------------------------------------------- */
 	{
-		displayName: 'ID',
-		name: 'id',
+		displayName: "ID",
+		name: "id",
 		required: true,
-		type: 'string',
-		default: '',
+		type: "string",
+		default: "",
 		displayOptions: {
 			show: {
-				resource: ['methods'],
-				operation: ['get'],
+				resource: ["methods"],
+				operation: ["get"],
 			},
 		},
 	},
-
+	{
+		displayName: "Optional parameters",
+		name: "optionalParameters",
+		type: "collection",
+		placeholder: "Add Options",
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ["methods"],
+				operation: ["get"],
+			},
+		},
+		options: [
+			{
+				displayName: "Locale",
+				name: "locale",
+				type: "options",
+				default: "",
+				options: localOptions,
+			},
+			{
+				displayName: "Currency",
+				name: "currency",
+				type: "string",
+				default: "",
+			},
+		],
+	},
 ];
